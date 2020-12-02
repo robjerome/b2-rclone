b0f5c23a809d3cd064618445f7d59f4a4cf66074
robjerome <github@robjerome.com>
2020-12-02 13:38:45 +0000

    Configurable sync/restore directory

--- restore.sh
+++ restore.sh
@@ -10,17 +10,18 @@ param()
   eval ${3}=$(cat "${1}" | grep -E "^${2}=" | sed "s|^${2}=||")
 }
 
-param "${B2_PARM}" "PASSPHRASE" B2_MAGIC
-param "${B2_PARM}" "BUCKET"     B2_BUCKET
-param "${B2_PARM}" "KEYID"      B2_KEYID
-param "${B2_PARM}" "APPKEY"     B2_APPKEY
+param "${B2_PARM}" "PASSPHRASE"  B2_MAGIC
+param "${B2_PARM}" "BUCKET"      B2_BUCKET
+param "${B2_PARM}" "KEYID"       B2_KEYID
+param "${B2_PARM}" "APPKEY"      B2_APPKEY
+param "${B2_PARM}" "DIRECTORY"   B2_DIRECTORY
 
-B2_DEST="${B2_ROOT}/bucket"
-B2_LIST="${B2_ROOT}/.sync"
-B2_SYNC="${B2_ROOT}/sync"
-B2_TEMP="${B2_ROOT}/.tmp"
+B2_SYNC_DIR="${B2_DIRECTORY:-$B2_ROOT}/sync"
+B2_SYNC_LST="${B2_DIRECTORY:-$B2_ROOT}/.sync"
+B2_REST_DIR="${B2_DIRECTORY:-$B2_ROOT}/restore"
+B2_REST_LST="${B2_DIRECTORY:-$B2_ROOT}/.restore"
 
-mkdir -p "${B2_SYNC}" && rm -f "${B2_LIST}" && touch "${B2_LIST}" && rm -f "${B2_TEMP}"
+mkdir -p "${B2_SYNC_DIR}" && rm -f "${B2_SYNC_LST}" && touch "${B2_SYNC_LST}" && rm -f "${B2_REST_LST}"
 
 RCLONE_CONF="${B2_ROOT}/rclone.conf"
 
@@ -38,39 +39,39 @@ if [ $# -eq 0 ] ; then
   printf "\nSpecify the file to restore or --full to restore everthing\n"
 elif [ $# -eq 1 ] && [ "${1}" = "--full" ] ; then
   printf "\nSyncing ...\n\n"
-  "${RCLONE}" ${RCLONE_FLAGS} --b2-versions ls "remote:${B2_BUCKET}" | awk '{for(i=2;i<NF;i++) printf $i " "; print $NF}' > "${B2_LIST}"
-  "${RCLONE}" ${RCLONE_FLAGS} sync --checksum "remote:${B2_BUCKET}" "${B2_SYNC}"
+  "${RCLONE}" ${RCLONE_FLAGS} --b2-versions ls "remote:${B2_BUCKET}" | awk '{for(i=2;i<NF;i++) printf $i " "; print $NF}' > "${B2_SYNC_LST}"
+  "${RCLONE}" ${RCLONE_FLAGS} sync --checksum "remote:${B2_BUCKET}" "${B2_SYNC_DIR}"
 else
   printf "\nSyncing ...\n\n"
-  "${RCLONE}" ${RCLONE_FLAGS} --b2-versions ls "remote:${B2_BUCKET}" | awk '{for(i=2;i<NF;i++) printf $i " "; print $NF}' > "${B2_TEMP}"
+  "${RCLONE}" ${RCLONE_FLAGS} --b2-versions ls "remote:${B2_BUCKET}" | awk '{for(i=2;i<NF;i++) printf $i " "; print $NF}' > "${B2_REST_LST}"
   for OBJECT in "$@" ; do
     OBJECT=$(echo "${OBJECT}" | sed "s|/$||")
-    FILE=$(grep -E "^${OBJECT}$" "${B2_TEMP}" | tee -a "${B2_LIST}")
+    FILE=$(grep -E "^${OBJECT}$" "${B2_REST_LST}" | tee -a "${B2_SYNC_LST}")
     if [ "${FILE}" != "" ] ; then
       printf "\n[file] ${OBJECT}\n\n"
-      "${RCLONE}" ${RCLONE_FLAGS} sync --checksum "remote:${B2_BUCKET}/${OBJECT}" "${B2_SYNC}/$(dirname "${FILE}")"
+      "${RCLONE}" ${RCLONE_FLAGS} sync --checksum "remote:${B2_BUCKET}/${OBJECT}" "${B2_SYNC_DIR}/$(dirname "${FILE}")"
     else
-      DIRECTORY=$(grep -E "^${OBJECT}" "${B2_TEMP}" | tee -a "${B2_LIST}")
+      DIRECTORY=$(grep -E "^${OBJECT}" "${B2_REST_LST}" | tee -a "${B2_SYNC_LST}")
       if [ "${DIRECTORY}" != "" ] ; then
         printf "\n[directory] ${OBJECT}\n\n"
-        "${RCLONE}" ${RCLONE_FLAGS} sync --checksum "remote:${B2_BUCKET}/${OBJECT}" "${B2_SYNC}/${OBJECT}"
+        "${RCLONE}" ${RCLONE_FLAGS} sync --checksum "remote:${B2_BUCKET}/${OBJECT}" "${B2_SYNC_DIR}/${OBJECT}"
       fi
     fi
   done
-  rm -f "${B2_TEMP}"
+  rm -f "${B2_REST_LST}"
 fi
 
-sed "s|^/||" -i "${B2_LIST}"
+sed "s|^/||" -i "${B2_SYNC_LST}"
 
-if [ $(cat "${B2_LIST}" | wc -l) -ne 0 ] ; then
+if [ $(cat "${B2_SYNC_LST}" | wc -l) -ne 0 ] ; then
   printf "Decrypting ...\n\n"
 fi
 
 while IFS= read -r LINE ; do
-   echo "${B2_DEST}/${LINE}"
-   mkdir -p "$(dirname "${B2_DEST}/${LINE}")"
-   openssl enc -d -aes256 -iter 1 -nosalt -pass "pass:${B2_MAGIC}" -in "${B2_SYNC}/${LINE}" -out "${B2_DEST}/${LINE}"
-done < "${B2_LIST}"
+   echo "${B2_REST_DIR}/${LINE}"
+   mkdir -p "$(dirname "${B2_REST_DIR}/${LINE}")"
+   openssl enc -d -aes256 -iter 1 -nosalt -pass "pass:${B2_MAGIC}" -in "${B2_SYNC_DIR}/${LINE}" -out "${B2_REST_DIR}/${LINE}"
+done < "${B2_SYNC_LST}"
 
 rm "${RCLONE_CONF}"
 
